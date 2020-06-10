d64057f373c2723913871ed1a7bea84215e3d774
natanaelkristiawan <natanaelkristiawan@hotmail.com>
2020-06-10 14:13:09 +0800

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,13 +8,13 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <div class="flex-center position-ref full-height" id="app">
            <secret-component :googleLink="{{ json_encode(route('public.googleLogin')) }}" :users="{{ Auth::check() == true ? 'true' : 'false' }}"></secret-component>
         </div>
 
-        <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
